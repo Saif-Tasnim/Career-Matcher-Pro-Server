f93bd6d6be0ac57c1792ec02a2349391fd45f91e
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-09-18 14:36:50 +0600

    data soring part done

--- index.js
+++ index.js
@@ -45,29 +45,59 @@ const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@clu
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.db("admin").command({ ping: 1 });
+        await client.connect();
+
+        const userCollection = client.db("career-matcher-pro").collection("user");
+
+        app.post("/user", verifyJWT, async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            const email = data.email;
+            const query = { email: email };
+            const find = await userCollection.findOne(query);
+
+            if (find) {
+                return res.send({ message: "User Already Registered" });
+            }
+
+            const result = await userCollection.insertOne(data);
+            res.send(result);
+
+        })
+
+
+        // jwt token
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.JSON_SECRET_KEY, { expiresIn: "1h" })
+            res.send({ token })
+
+        })
+        
+        // Send a ping to confirm a successful connection
+        
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log("Server is running in bg")
 })
